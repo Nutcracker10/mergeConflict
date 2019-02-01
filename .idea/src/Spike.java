fddf0f361dbdca36dfc57b8f9cba82dd76c380fc
Caoimhe Tiernan <caoimhe.tiernan.work@gmail.com>
2019-02-01 10:47:37 +0000

    May have deleted file prematurely. Can use this to track the number of the spike?? possibly

---
+++ .idea/src/Spike.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+//This class represemts the Spikes on the board where the checkers move. They act like a stack.
+// Checkers  can move from one spike to another. Store Spike objects in LinkedSist???
+
+public class Spike
+{
+
+    public Spike()
+    {
+
+    }
+
+
+
+
+
+}
