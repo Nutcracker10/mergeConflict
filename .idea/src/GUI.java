4fdf30a1e14963a499ae1ebe28ba6fc8891f9e77
James Kirwan <jamesgkirwan@gmail.com>
2019-02-01 11:38:09 +0000

    MAIN WORKS!

--- .idea/src/GUI.java
+++ .idea/src/GUI.java
@@ -4,15 +4,8 @@ import java.awt.*;
 
 public class GUI{
     public static void main(String[] args) {
-       JFrame f = new JFrame();
-       EastPanel e = new EastPanel();
 
-       f.setLayout(new BorderLayout());
-       f.add(e, BorderLayout.EAST);
-
-       f.setSize(800, 600);
-       f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-       f.setVisible(true);
+      new frame();
 
     }
 
