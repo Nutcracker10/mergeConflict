fc04fe6b1b78414617d3362dbdbd348e78056706
James Kirwan <jamesgkirwan@gmail.com>
2019-02-01 11:34:09 +0000

    Main works now, but temporary

--- .idea/src/GUI.java
+++ .idea/src/GUI.java
@@ -1,10 +1,19 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUI{
     public static void main(String[] args) {
+       JFrame f = new JFrame();
+       EastPanel e = new EastPanel();
 
-       new frame();
+       f.setLayout(new BorderLayout());
+       f.add(e, BorderLayout.EAST);
+
+       f.setSize(800, 600);
+       f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       f.setVisible(true);
+
     }
 
 
--- .idea/src/frame.java
+++ .idea/src/frame.java
@@ -3,10 +3,11 @@ import java.awt.*;
 
 public class frame extends JFrame {
 
-    public EastPanel east;
+    EastPanel east = new EastPanel();
 
     frame() {
         this.setSize(800, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.setVisible(true);
         this.add(east, BorderLayout.EAST);
