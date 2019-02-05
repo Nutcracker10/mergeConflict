02c3c317910c6b195c45b168b32de4124a012405
James Kirwan <jamesgkirwan@gmail.com>
2019-02-05 13:51:23 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/src/CentrePanel.java
    #	.idea/src/Resources/Backgammon_board_-_03.jpg
    #	.idea/src/Resources/images/Backgammon_board_-_03.jpg
    #	Backgammon_board_-_03.jpg

--- .idea/src/CentrePanel.java
+++ .idea/src/CentrePanel.java
@@ -1,10 +1,7 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class CentrePanel extends JPanel {
 
-    Toolkit toolkit = Toolkit.getDefaultToolkit();
-    Image board = toolkit.getImage("Backgammon_board_-_03.jpg");
 
 
 }
