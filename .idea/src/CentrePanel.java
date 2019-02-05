68f87183bc1ae4035a1d069bb19528848a9ebc8c
James Kirwan <jamesgkirwan@gmail.com>
2019-02-05 14:08:39 +0000

    clearing commit

--- .idea/src/CentrePanel.java
+++ .idea/src/CentrePanel.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
+import javax.imageio.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class CentrePanel extends JPanel {
+    BufferedImage board = ImageIO.read("Backgammon_board_-_03.jpg");
 
-    Toolkit toolkit = Toolkit.getDefaultToolkit();
-    Image board = toolkit.getImage("Backgammon_board_-_03.jpg");
+    ImageIcon boardicon = new ImageIcon(board);
+
+
+
+
+    CentrePanel() {
+        JLabel boardlabel = new JLabel();
+        boardlabel.setIcon(boardicon);
+        this.add(boardlabel);
+    }
 
 
 }
