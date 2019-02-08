e8a33eaac7af06ba7210574766cd04a9e66d13c3
James Kirwan <jamesgkirwan@gmail.com>
2019-02-08 13:09:27 +0000

    Fixed weird merge issue

--- .idea/src/CentrePanel.java
+++ .idea/src/CentrePanel.java
@@ -14,34 +14,22 @@ public class CentrePanel extends JPanel
       
     CentrePanel() throws IOException 
     {
-    	try
-        {
-    		image =  ImageIO.read(CentrePanel.class.getResource("/resources/images/Backgammon_board_-_03.jpg")); //gets backgammon board image
+
+        this.setSize(400, 300);
+
+        try {
+            image =  ImageIO.read(CentrePanel.class.getResource("/resources/images/Backgammon_board_smaller.jpg")); //gets backgammon board image
         }
-        catch (IOException ioe)
-    	{
-        	ioe.printStackTrace();
+        catch (IOException ioe) {
+            ioe.printStackTrace();
         }
 
-        //default constructor
-        CentrePanel() throws IOException {
-
-            this.setSize(400, 300);
-
-            try
-            {
-                image =  ImageIO.read(CentrePanel.class.getResource("/resources/images/Backgammon_board_smaller.jpg")); //gets backgammon board image
-            }
-            catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-
-           JLabel label = new JLabel(new ImageIcon(image)); // assigns image to a Label
-           this.add(label);            // adds image to panel
+        JLabel label = new JLabel(new ImageIcon(image)); // assigns image to a Label
+        this.add(label);            // adds image to panel
 
         }
 
-    }
-}
+}// end of Centre Panel
+
 
 
