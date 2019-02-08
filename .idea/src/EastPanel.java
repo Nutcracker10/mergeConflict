e53b5ddbac58fc4e44a50fb7c627f326c719935b
James Kirwan <jamesgkirwan@gmail.com>
2019-02-08 13:31:00 +0000

    Fixing pulling team issue

--- .idea/src/EastPanel.java
+++ .idea/src/EastPanel.java
@@ -5,13 +5,16 @@ public class EastPanel extends JPanel {
 
     JTextField enterText = new JTextField();                     // a field for entering details
     JTextArea areaText = new JTextArea("Welcome to Backgammon"); // an area for displaying game details
-    JTextArea playerName = new JTextArea("Player: ");         // Displays player name
+    JScrollPane scrollPane = new JScrollPane(areaText);          // adds a scroll bar to the text area
+    JTextArea playerName = new JTextArea("Player: ");            // Displays player name
     JTextArea playerScore = new JTextArea("Score: ");            // Displays Player Score
     JPanel subpanel = new JPanel();
 
     EastPanel(){
         this.setLayout(new BorderLayout());   // sets border layout to Eastpanel
         subpanel.setLayout(new GridLayout(2, 1)); // sets grid layout to subpanel
+
+        areaText.setEditable(false);    // makes the text area uneditible
 
         //adds each component to panel
         subpanel.add(playerName);
