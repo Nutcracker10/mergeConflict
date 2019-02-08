0ca8bfa5a4d04265f54fc7a9fd0b939faf75ae0d
James Kirwan <jamesgkirwan@gmail.com>
2019-02-08 14:06:51 +0000

    Added a working text field

--- .idea/src/EastPanel.java
+++ .idea/src/EastPanel.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class EastPanel extends JPanel {
+public class EastPanel extends JPanel implements ActionListener {
 
     JTextField enterText = new JTextField();                     // a field for entering details
-    JTextArea areaText = new JTextArea("Welcome to Backgammon"); // an area for displaying game details
+    JTextArea areaText = new JTextArea("Welcome to Backgammon\n"); // an area for displaying game details
     JTextArea playerName = new JTextArea("Player: ");         // Displays player name
     JTextArea playerScore = new JTextArea("Score: ");            // Displays Player Score
     JPanel subpanel = new JPanel();
@@ -12,6 +14,8 @@ public class EastPanel extends JPanel {
     EastPanel(){
         this.setLayout(new BorderLayout());   // sets border layout to Eastpanel
         subpanel.setLayout(new GridLayout(2, 1)); // sets grid layout to subpanel
+
+        enterText.addActionListener(this);
 
         //adds each component to panel
         subpanel.add(playerName);
@@ -25,4 +29,12 @@ public class EastPanel extends JPanel {
         playerScore.setBorder(BorderFactory.createLineBorder(Color.BLACK));   // creates line around score
     }
 
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+        String text = enterText.getText();
+        areaText.append(text + "\n");
+        enterText.selectAll();
+
+    }
 }
