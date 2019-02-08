0a9434e5be4593feb84392a4b30e276664519cf0
James Kirwan <jamesgkirwan@gmail.com>
2019-02-08 16:51:20 +0000

    prevented wrong fields from being edited

--- .idea/src/EastPanel.java
+++ .idea/src/EastPanel.java
@@ -5,15 +5,21 @@ import java.awt.event.ActionListener;
 
 public class EastPanel extends JPanel implements ActionListener {
 
-    JTextField enterText = new JTextField();                     // a field for entering details
+    JTextField enterText = new JTextField();                       // a field for entering details
     JTextArea areaText = new JTextArea("Welcome to Backgammon\n"); // an area for displaying game details
-    JTextArea playerName = new JTextArea("Player: ");         // Displays player name
-    JTextArea playerScore = new JTextArea("Score: ");            // Displays Player Score
-    JPanel subpanel = new JPanel();
+    JTextArea playerName = new JTextArea("Player: ");              // Displays player name
+    JTextArea playerScore = new JTextArea("Score: ");              // Displays Player Score
+    JPanel subpanel = new JPanel();                                // a subpanel for buttons
+    JScrollPane scrollPane = new JScrollPane(areaText);                    // adds scrolling functionality to the text area
 
     EastPanel(){
         this.setLayout(new BorderLayout());   // sets border layout to Eastpanel
         subpanel.setLayout(new GridLayout(2, 1)); // sets grid layout to subpanel
+
+        // prevents user from typing in these areas
+        areaText.setEditable(false);
+        playerScore.setEditable(false);
+        playerName.setEditable(false);
 
         enterText.addActionListener(this);
 
@@ -34,12 +40,13 @@ public class EastPanel extends JPanel implements ActionListener {
 
         String text = enterText.getText();
 
-        if(areaText.getText() == "quit" || enterText.getText() == "Quit"){
+        if(text == "quit"){
             System.exit(0);
         }
+        else {
+            areaText.append(text + "\n");
+            enterText.selectAll();
+        }
 
-        areaText.append(text + "\n");
-        enterText.selectAll();
-
-    }
+    }//end of actionPerformed
 }
