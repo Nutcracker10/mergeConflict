66decc9d83838a89a27202d23b5ca8d6b57780d8
Saoirse Houlihan <saoirse.houlihan@ucdconnect.ie>
2019-02-08 13:35:44 +0000

    Merge branch 'master' of https://github.com/UCD-COMP20050/mergeConflict.git into Saoirse
    Conflicts:
    	.idea/src/CentrePanel.java

--- .idea/src/CentrePanel.java
+++ .idea/src/CentrePanel.java
@@ -8,17 +8,16 @@ import java.io.IOException;
 public class CentrePanel extends JPanel
 {
     private BufferedImage image;  //gets backgammon board image
-    WhiteChecker white = new WhiteChecker();
+    WhiteChecker white = new WhiteChecker(0, 0);
+    BlackChecker black = new BlackChecker(50, 0);
+
 
     //default constructor
-      
     CentrePanel() throws IOException 
     {
-        this.setSize(400, 300);
-
         try
         {
-            image =  ImageIO.read(CentrePanel.class.getResource("/resources/images/Backgammon_board_smaller.jpg")); //gets backgammon board image
+        	image =  ImageIO.read(CentrePanel.class.getResource("/resources/images/Backgammon_board_smaller.jpg")); //gets backgammon board image
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
@@ -27,9 +26,10 @@ public class CentrePanel extends JPanel
         JLabel label = new JLabel(new ImageIcon(image)); // assigns image to a Label
         this.add(label);            // adds image to panel
 
-    }
 
+     }
 }
+
 
 
 
