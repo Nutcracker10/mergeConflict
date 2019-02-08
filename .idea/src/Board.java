d0e8ffcbd57527a79e8654e0c8ca30325d2a3342
Saoirse Houlihan <saoirse.houlihan@ucdconnect.ie>
2019-02-08 21:07:27 +0000

    THE COMPLETED BOARD, SHE IS BEAUTIFUL

--- .idea/src/Board.java
+++ .idea/src/Board.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import javax.swing.*;
 
 public class Board extends Canvas
 {
@@ -9,8 +8,8 @@ public class Board extends Canvas
 		drawBackground(g);
 		drawBoard(g);
 		drawBearOff(g);
-		//drawTrianglesOne(g);
-		//drawTrianglesTwo(g);
+		drawTrianglesOne(g);
+		drawTrianglesTwo(g);
 		
 	}
 	
@@ -48,14 +47,88 @@ public class Board extends Canvas
 	
 	private void drawTrianglesOne(Graphics g)
 	{
-		//TODO
 		//Draw all black triangles
+		int x1[] = {51, 101, 76};
+		int y1[] = {50, 50, 260};
+		g.setColor(Color.BLACK);
+		g.fillPolygon(x1, y1, 3);
+		
+		int x2[] = {151, 201, 176};
+		g.fillPolygon(x2, y1, 3);
+		
+		int x3[] = {251, 301, 276};
+		g.fillPolygon(x3, y1, 3);
+		
+		int x4[] = {403, 453, 428};
+		g.fillPolygon(x4, y1, 3);
+		
+		int x5[] = {503, 553, 528};
+		g.fillPolygon(x5, y1, 3);
+		
+		int x6[] = {603, 653, 628};
+		g.fillPolygon(x6, y1, 3);
+		
+		int y2[] = {550, 550, 340};
+		int x7[] = {101, 151, 126};
+		g.fillPolygon(x7, y2, 3);
+		
+		int x8[] = {201, 251, 226};
+		g.fillPolygon(x8, y2, 3);
+		
+		int x9[] = {301, 351, 326};
+		g.fillPolygon(x9, y2, 3);
+		
+		int x10[] = {453, 503, 478};
+		g.fillPolygon(x10, y2, 3);
+		
+		int x11[] = {553, 603, 578};
+		g.fillPolygon(x11, y2, 3);
+		
+		int x12[] = {653, 703, 678};
+		g.fillPolygon(x12, y2, 3);
 	}
 	
 	private void drawTrianglesTwo(Graphics g)
 	{
-		//TODO
 		//Draw all white triangles
+		g.setColor(Color.WHITE);
+		int y1[] = {50, 50, 260};
+		int x1[] = {101, 151, 126};
+		g.fillPolygon(x1, y1, 3);
+		
+		int x2[] = {201, 251, 226};
+		g.fillPolygon(x2, y1, 3);
+		
+		int x3[] = {301, 351, 326};
+		g.fillPolygon(x3, y1, 3);
+		
+		int x4[] = {453, 503, 478};
+		g.fillPolygon(x4, y1, 3);
+		
+		int x5[] = {553, 603, 578};
+		g.fillPolygon(x5, y1, 3);
+		
+		int x6[] = {653, 703, 678};
+		g.fillPolygon(x6, y1, 3);
+		
+		int y2[] = {550, 550, 340};
+		int x7[] = {51, 101, 76};
+		g.fillPolygon(x7, y2, 3);
+		
+		int x8[] = {151, 201, 176};
+		g.fillPolygon(x8, y2, 3);
+		
+		int x9[] = {251, 301, 276};
+		g.fillPolygon(x9, y2, 3);
+		
+		int x10[] = {403, 453, 428};
+		g.fillPolygon(x10, y2, 3);
+		
+		int x11[] = {503, 553, 528};
+		g.fillPolygon(x11, y2, 3);
+		
+		int x12[] = {603, 653, 628};
+		g.fillPolygon(x12, y2, 3);
 	}
 	
 }
