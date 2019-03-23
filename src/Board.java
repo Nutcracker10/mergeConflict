57d298966758123e1e1201c2be7aebddd991efe2
Caoimhe Tiernan <caoimhe.tiernan.work@gmail.com>
2019-03-23 11:03:11 +0000

    commiting beofre pull

--- src/Board.java
+++ src/Board.java
@@ -720,6 +720,46 @@ public class Board extends JPanel
 
 			if(pips[colour][25] > 0)//TODO bar possible moves for black
 			{
+                if(!(pips[oppositeColour][rolls[0] + 1] >= 1))
+                {
+                    addNums = 25 - rolls[0];
+                    possibleMoves += "\n" + "bar" + "-" + addNums;
+
+                }
+               else if((pips[oppositeColour][rolls[0] + 1] == 1))
+                {
+                    addNums = 25 - rolls[0];
+                    possibleMoves += "\n" + "bar" + "-" + addNums + "*";
+
+                }
+
+                if(!(pips[oppositeColour][rolls[1] + 1] >= 1))
+                {
+                    addNums = 25 - rolls[1];
+                    possibleMoves += "\n" + "bar" + "-" + addNums;
+
+                }
+                else if((pips[oppositeColour][rolls[1] + 1] == 1))
+                {
+                    addNums = 25 - rolls[1];
+                    possibleMoves += "\n" + "bar" + "-" + addNums + "*";
+
+                }
+
+                if(!(pips[oppositeColour][rolls[0]+rolls[1] + 1] >= 1))
+                {
+                    addNums = 25 - rolls[1] - rolls[0];
+                    possibleMoves += "\n" + "bar" + "-" + addNums;
+
+                }
+                else if((pips[oppositeColour][rolls[0]+rolls[1] + 1] == 1))
+                {
+                    addNums = 25 - rolls[1] - rolls[0];
+                    possibleMoves += "\n" + "bar" + "-" + addNums + "*";
+
+                }
+
+
 
 
 
