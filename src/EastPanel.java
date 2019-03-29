1972b83a762affa377ff2d0e57f942360dfaf3f7
Saoirse Houlihan <saoirse.houlihan@ucdconnect.ie>
2019-03-29 10:31:43 +0000

    Fixed bug where white moved checker from bear off instead of bar

--- src/EastPanel.java
+++ src/EastPanel.java
@@ -242,6 +242,7 @@ public class EastPanel extends JPanel implements ActionListener, Scrollable{
                 try {
 						if (string.contains("Bar")) {
 							from = 25;
+							moveToReturn[0] = from;
 						}
 						else {
 							firstHalf = firstHalf.replaceAll("-", "");
@@ -250,6 +251,7 @@ public class EastPanel extends JPanel implements ActionListener, Scrollable{
 						}
 						if(string.contains("Off")) {
 							to = 0;
+							moveToReturn[1] = to;
 						}
 						else {
 							String secondHalf = string.substring(string.indexOf("-") + 1);
@@ -354,7 +356,7 @@ public class EastPanel extends JPanel implements ActionListener, Scrollable{
 
 		String[] s = board.acceptableMoves(colour, result).split("\\n");
 
-		if(s[0] == "") {
+		if(s[1] == "") {
 			areaText.append("No turns available, starting next turn\n");
 			try {
 				Thread.sleep(1000); // causes the program to sleep for 1 second
